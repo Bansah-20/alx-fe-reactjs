8eecca157882ebd0ad72a4e1bbf8cd6ac29fd0ef
Kelvin Abiram Bansah <yobowan788@gmail.com>
2025-09-01 20:33:08 +0000

    Added Userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
@@ -35,6 +36,9 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" 
+      age="25" 
+      bio="Loves hiking and photography" />
       <Footer />
     </>
   )
