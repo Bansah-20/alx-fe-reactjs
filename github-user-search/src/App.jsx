5b4c93ca5055bb97e272325a3b3d055e4256d1b0
Kelvin Abiram Bansah <yobowan788@gmail.com>
2025-09-22 13:27:32 +0000

    Integrate GitHub API and add basic user search feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import SearchBar from "./components/SearchBar";
-import UserCard from "./components/UserCard";
+import Search from "./components/Search";
 
 function App() {
-  const [user, setUser] = useState(null);
-
   return (
     <div className="app">
       <h1>GitHub User Search</h1>
-      <SearchBar setUser={setUser} />
-      {user && <UserCard user={user} />}
+      <Search />
     </div>
   );
 }
